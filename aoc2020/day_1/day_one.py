52d0c9b357b61ecae248eba81c5e04db5170c865
Moritz Schillinger <28610231+schilli91@users.noreply.github.com>
2020-12-26 12:57:53 +0100

    Add day one of 2020

--- aoc2020/day_1/day_one.py
+++ aoc2020/day_1/day_one.py
@@ -1,9 +1,38 @@
+from aoc2020.day_1.expense_report import expense_report
 
 
+def fix_expense_report(expense_report, num_summand):
+    if num_summand == 2:
+        return fix_expense_report_two_summands(expense_report)
 
-def fix_expense_report(expense_report):
-    pass
+    if num_summand == 3:
+        return fix_expense_report_three_summands(expense_report)
+
+
+def fix_expense_report_two_summands(expense_report):
+    for i in expense_report:
+        for j in expense_report:
+            if (i + j == 2020):
+                return i * j
+
+    return False
+
+
+def fix_expense_report_three_summands(expense_report):
+    for i in expense_report:
+        for j in expense_report:
+            for k in expense_report:
+                if i == j or i == j or j == k:
+                    continue
+                if (i + j + k == 2020):
+                    return i * j * k
+
+    return False
 
 
 if __name__ == '__main__':
     print("Day 1")
+    res = fix_expense_report(expense_report, 2)
+    print(f"The expense report result with two summands is {res}.")
+    res = fix_expense_report(expense_report, 3)
+    print(f"The expense report result with three summands is {res}.")
--- aoc2020/day_1/test_day_one.py
+++ aoc2020/day_1/test_day_one.py
@@ -1,5 +1,7 @@
 import pytest
 
+from aoc2020.day_1.day_one import fix_expense_report
+
 sample_expense_report = [
     1721,
     979,
@@ -11,9 +13,11 @@ sample_expense_report = [
 
 
 def test_fix_expense_report():
-    #res = fix_expense_report(sample_expense_report)
-    res = 0
+    res = fix_expense_report(sample_expense_report, 2)
     assert 514579 == res
+    res = fix_expense_report(sample_expense_report, 3)
+    assert 241861950 == res
+
 
 if __name__ == '__main__':
     print("Day 1")
